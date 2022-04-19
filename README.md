b20c4d4ad80870ec4b34405f827b46dbe34c5208
Favour Orji <favouru077@gmail.com>
2022-04-19 15:25:44 -0700

    printf function

---
+++ README.md
@@ -0,0 +1,1 @@
+Prints a string to the standard output, according to a given format
---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+typedef struct printHandler
+{
+	char c;
+
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+int _putchar(char c);
+int _puts(char *str);
+int (*get_print(char s))(va_list, flags_t *);
+int _printf(const char *format, ...);
+int get_flag(char s, flags_t *f);
+
+#endif
