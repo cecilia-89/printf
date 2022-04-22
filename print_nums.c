2abea1943e40fafdc782b8e81136ed65fc315332
Favour Orji <favouru077@gmail.com>
2022-04-22 12:36:36 -0700

    printf task

--- print_nums.c
+++ print_nums.c
@@ -1,26 +1,47 @@
 #include "main.h"
 
+
+
 /**
+
  * print_int - prints an integer
+
  * @l: va_list of arguments from _printf
+
  * @f: pointer to the struct flags determining
+
  * if a flag is passed to _printf
+
  * Return: number of char printed
+
  */
 
 int print_int(va_list l, flags_t *f)
+
 {
+
 	int n = va_arg(l, int);
+
 	int res = count_digit(n);
 
+
+
 	if (f->space == 1 && f->plus == 0 && n >= 0)
+
 		res += _putchar(' ');
+
 	if (f->plus == 1 && n >= 0)
+
 		res += _putchar('+');
+
 	if (n <= 0)
+
 		res++;
+
 	print_number(n);
+
 	return (res);
+
 }
 
 
@@ -39,7 +60,7 @@ int print_int(va_list l, flags_t *f)
 
  */
 
-/**int print_unsigned(va_list l, flags_t *f)
+int print_unsigned(va_list l, flags_t *f)
 
 {
 
@@ -54,7 +75,7 @@ int print_int(va_list l, flags_t *f)
 	return (_puts(str));
 
 }
-*/
+
 
 
 /**
@@ -113,7 +134,7 @@ void print_number(int n)
 
  */
 
-/**int count_digit(int i)
+int count_digit(int i)
 
 {
 
@@ -143,4 +164,4 @@ void print_number(int n)
 
 	return (d);
 
-}*/
+}
